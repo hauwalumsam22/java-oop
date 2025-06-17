b815b16c11cd3a095e165662741b337138aa9cde
HAUWA LUMSAM <149111359+hauwalumsam22@users.noreply.github.com>
2025-06-17 13:48:19 +0100

    Add files via upload

---
+++ OOPJAVA/Arrayexample.js
@@ -0,0 +1,5 @@
+packagebasics;
+
+public class Arrayexample{
+
+}
---
+++ OOPJAVA/src/Arrayexample.java
@@ -0,0 +1,16 @@
+
+
+public class Arrayexample {
+    public static void main(String[] args){
+
+        int[] myArray = {1,3,5,7,9};
+        int[] anotherArray = new int[10];
+        String[] gender = {"Female", "Male"};
+
+
+        for (int i = 0; i < myArray.length; i++){
+            system.out.println("item at;" +i+ "is" +anotherArray);
+            anotherArray[i] = input.nextInt();
+        }
+    }
+}
